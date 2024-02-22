12065249e29e646443eaed0744004ee49437ffc1
jordi <jordi_f@apple.com>
2024-02-22 19:12:09 +0100

    Svelte app working?

---
+++ docs/_app/immutable/chunks/paths.DAqb29mz.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_qqmaqn)==null?void 0:s.base)??"/recetas";var a;const t=((a=globalThis.__sveltekit_qqmaqn)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ docs/_app/immutable/entry/start.CyOergBk.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DkMh0mkG.js";export{t as start};
